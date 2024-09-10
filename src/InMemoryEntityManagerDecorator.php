7e3036f3afee28e351b07710ada8baf96ebf7a3d
prophetz <kpuzuc@gmail.com>
2024-09-10 04:09:01 +0300

    fix

--- src/InMemoryEntityManagerDecorator.php
+++ src/InMemoryEntityManagerDecorator.php
@@ -2,12 +2,25 @@
 
 namespace Firehed\Mocktrine;
 
+use Doctrine\ORM\Configuration;
 use Doctrine\ORM\Decorator\EntityManagerDecorator;
+use Doctrine\ORM\EntityManagerInterface;
 
 class InMemoryEntityManagerDecorator extends EntityManagerDecorator
 {
-    public function __construct(InMemoryEntityManager $inMemoryEntityManager)
-    {
+    private EntityManagerInterface $originalEntityManager;
+
+    public function __construct(
+        InMemoryEntityManager $inMemoryEntityManager,
+        EntityManagerInterface $originalEntityManager
+    ) {
         parent::__construct($inMemoryEntityManager);
+
+        $this->originalEntityManager = $originalEntityManager;
+    }
+
+    public function getConfiguration(): Configuration
+    {
+        return $this->originalEntityManager->getConfiguration();
     }
 }
